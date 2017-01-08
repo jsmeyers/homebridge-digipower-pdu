b4d7ffdd8cf866e788d1e07e1f096342f7e5f2a9
jsmeyers <jsmeyers@users.noreply.github.com>
2017-01-08 18:42:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ class PDUAccessory {
 	constructor(log, config) {
 		this.log = log;
 		this.services = [];
-		var portcount = snmp.createSession(config.ip, config.snmp_community);
+		this.portcount = snmp.createSession(config.ip, config.snmp_community);
 		var switch_oid = '1.3.6.1.2.1.2.1.0';
-		portcounted  = portcount.get([switch_oid])
+		portcounted  = this.portcount.get([switch_oid])
 			.then(varbinds => {
 				console.log.info(varbinds);
 				var switches = varbinds[0].value.toString().split(',');
